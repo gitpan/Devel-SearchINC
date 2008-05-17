74dc90d206b3e7ebcc42cfd7491effb6a983920e
Marcel Grünauer <marcel@cpan.org>
2008-05-17 14:09:39 +0000

    Import of MARCEL/Devel-SearchINC-1.33 from CPAN.
    gitpan-cpan-distribution: Devel-SearchINC
    gitpan-cpan-version:      1.33
    gitpan-cpan-path:         MARCEL/Devel-SearchINC-1.33.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Devel/SearchINC.pm
+++ lib/Devel/SearchINC.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 
-our $VERSION = '1.32';
+our $VERSION = '1.33';
 
 
 BEGIN {
@@ -91,6 +91,8 @@ sub import {
 1;
 
 __END__
+
+
 
 =head1 NAME
 
@@ -194,12 +196,16 @@ directories.
 If you talk about this module in blogs, on del.icio.us or anywhere else,
 please use the C<develsearchinc> tag.
 
+=head1 VERSION 
+                   
+This document describes version 1.33 of L<Devel::SearchINC>.
+
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests to
-C<bug-devel-searchinc@rt.cpan.org>, or through the web interface at
+C<<bug-devel-searchinc@rt.cpan.org>>, or through the web interface at
 L<http://rt.cpan.org>.
 
 =head1 INSTALLATION
@@ -212,16 +218,17 @@ The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
 site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2002-2007 by Marcel GrE<uuml>nauer
+Copyright 2004-2008 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
+
 
 =cut
 
